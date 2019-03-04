00d189e3b5555bbee7632f1ebfad4dc86740227b
Jordan Epps <jordanepps@gmail.com>
2019-03-04 14:10:03 -0500

    Create route endpoints and modularize code

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const winston = require('winston');
 const { NODE_ENV } = require('./config');
+const cardRouter = require('./card/card-router');
+const listRouter = require('./list/list');
 
 const app = express();
 
@@ -12,6 +15,22 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
+
+app.use(function validateBearerToken(req, res, next) {
+	const apiToken = process.env.API_TOKEN;
+	const authToken = req.get('Authorization');
+
+	if (!authToken || authToken.split(' ')[1] !== apiToken) {
+		logger.error(`Unauthorized request to path : ${req.path}`);
+		return res.status(401).json({ error: 'Unauthorized request' });
+	}
+	// move to the next middleware
+	next();
+});
+
+app.use(cardRouter);
+app.use(listRouter);
 
 app.get('/', (req, res) => {
 	res.send('Hello, boilerplate!');
